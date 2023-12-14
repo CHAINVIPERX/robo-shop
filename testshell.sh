775a139ddc922efeb6afd9f98140f367bcb3d5a5
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-14 13:10:56 +0530

    date

--- testshell.sh
+++ testshell.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-p1=alekhya
-p2=balaji
+#p1=alekhya
+#p2=balaji
 
-echo "$p1:I Love You"
-echo "$p2:Okay.Love you too"
-echo "$p1:Its a Prank"
+#echo "$p1:I Love You"
+#echo "$p2:Okay.Love you too"
+#echo "$p1:Its a Prank
+
+DATE=$(date)
+echo "Date and time is ${DATE}"
