9fec788f0ad672c741526c37a5d86a55730c97b3
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-14 20:02:11 +0530

    q

--- testshell.sh
+++ testshell.sh
@@ -27,24 +27,34 @@
 #echo "${FRUITS[@]}"
 
 #number=$1
-#if [ $number -gt 50 ] 
-#then 
+#if [ $number -gt 50 ]
+#then
 #    echo " The number $number is greater than 50"
 #else
 #   echo "The number $number is smaller than 50"
 #fi
 
+VALIDATION() {
+    if [ $? -eq 0 ]; then
+        echo "Installation Successful"
+    else
+        echo "Installation Failed"
+        exit 1
+    fi
+}
+
 ID=$(id -u)
-if [ $ID -eq 0 ]
-then echo "ROOT User"
-else echo " Not a ROOT User"
-exit 1
+if [ $ID -eq 0 ]; then
+    echo "ROOT User"
+else
+    echo " Not a ROOT User"
+    exit 1
 fi
 
-yum install mysqlgdd -y
+yum install mysql -y
 
-if [ $? -eq 0 ]
-then echo "Installation Successful"
-else echo "Installation Failed"
-exit 1
-fi
+VALIDATION
+
+yum install git -y
+
+VALIDATION
