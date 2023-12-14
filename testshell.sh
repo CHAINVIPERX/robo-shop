b99af727df9c0ccbe50de605995df038b1c1e305
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-14 19:02:58 +0530

    UPDATE

--- testshell.sh
+++ testshell.sh
@@ -20,9 +20,22 @@
 #SUM=$(($n1+$n2))
 #echo "sum of $n1 and $n2 is $SUM"
 
-FRUITS=("JACKFRUIT" "ORANGE" "CAPPLE")
-echo "${FRUITS[0]}"
-echo "${FRUITS[1]}"
-echo "${FRUITS[2]}"
-echo "${FRUITS[@]}"
+#FRUITS=("JACKFRUIT" "ORANGE" "CAPPLE")
+#echo "${FRUITS[0]}"
+#echo "${FRUITS[1]}"
+#echo "${FRUITS[2]}"
+#echo "${FRUITS[@]}"
 
+#number=$1
+#if [ $number -gt 50 ] 
+#then 
+#    echo " The number $number is greater than 50"
+#else
+#   echo "The number $number is smaller than 50"
+#fi
+
+ID=$(id -u)
+if [ $ID -eq 0 ]
+then echo "ROOT User"
+else echo " Not a ROOT User"
+fi
