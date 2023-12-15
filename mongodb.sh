e7b584a56b51c18b69a22aa91c64cc4157e742b1
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-15 17:13:38 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -5,10 +5,45 @@ CG="\e[32m"
 CY="\e[33m"
 CN="\e[0m"
 
+VALIDATION(){
+    if [ $1 -eq 0 ]; 
+    then echo " Installation of $2 is SUCCESS " 
+        exit 1 
+    else echo " Installation of $2 has FAILED"
+    fi
+}
+
 ID=$(id -u)
-if [ $ID -ne 0 ]; then
+if [ $ID -ne 0 ]; 
+then
     echo -e "Please Run this Script as $CY ROOT USER $CN"
     exit 1
 else
     echo -e " $CG ROOT USER $CN"
 fi
+
+echo -e "installing $CG mongodb $CN"
+
+dnf install mongodb -y
+
+VALIDATION $? Mongodb
+
+if [ ${VALIDATION[0]} -eq 1 ]; 
+then
+    echo "Creating Mongodb Repo"
+    cp mongo.repo /etc/yum.repos.d/
+    echo "installing mongodb"
+    dnf install mongodb-org -y
+    VALIDATION $? Mongodb
+    exit 1
+fi
+
+echo " Configuring Mongodb"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+echo "Enabling Mongod"
+systemctl enable mongod
+echo "Starting Mongod"
+systemctl start Mongod
+
+netstat -lntp
