295e1cc8e9220e5ec4c4e2f7bde3a4d096151180
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-19 18:51:07 +0530

    q

--- testshell.sh
+++ testshell.sh
@@ -90,3 +90,40 @@
 #for i in {1..1054}; do
 #    echo "$i"
 #done
+
+<<comment
+echo " enter d to see date"
+echo " enter pd to see present working directory"
+
+read -r RESPONSE 
+
+caseresponse="${RESPONSE,,}"
+
+case $caseresponse in
+
+d) date;;
+pd) pwd;;
+*) echo "Invalid input";;
+esac
+comment
+<<comment
+read -rp "Enter 2 numbers" A B
+read -rp "Enter a for addition , s for substraction" RESPONSE
+addition()
+{
+    answer=$(($1+$2));
+    echo " $1 + $2 is = ${answer}"
+}
+substraction()
+{
+   #let answer=$1-$2
+   echo " $1-$2 is $(($1-$2))"
+}
+
+caseresponse="${RESPONSE,,}"
+case $caseresponse in
+a) addition "$A" "$B" ;;
+s) substraction "$A" "$B" ;;
+*) echo "Invalid input" ;;
+esac
+comment
