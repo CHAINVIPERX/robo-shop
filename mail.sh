9baa9a426243d9c9d74777ce6bf6271052e17db1
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-20 15:34:24 +0530

    q

--- mail.sh
+++ mail.sh
@@ -4,6 +4,6 @@ SUBJECT=$1
 TO_TEAM=$2
 BODY=$3
 TO_ADDRESS=$4
-
-EMAIL=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/BODY/$BODY/g"  mail.html)
+E_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
+EMAIL=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/BODY/$E_BODY/g"  mail.html)
 echo "$EMAIL" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
