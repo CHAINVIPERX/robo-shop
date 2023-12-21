30b226255f8e1b21cf84c32209adb424d288d0e1
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-21 15:26:04 +0530

    instances

--- instances.sh
+++ instances.sh
@@ -9,12 +9,12 @@ for ((i=0;i < ${#T_2[*]};i++))
 do
     echo "Creating ${T_2[$i]} Instance"
     PrivateIpAddress=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --security-group-ids ${SEC_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${T_2[$i]}}]" --query 'Instance[0].PrivateIpAddress' --output text)
-    echo "DONE:::${T_2[$i]:PrivateIpAddress}"
+    echo "DONE:::${T_2[$i]}:${PrivateIpAddress}"
 done
 
 for ((i=0;i < ${#T_3[*]};i++))
 do
     echo "Creating ${T_3[$i]} Instance"
     PrivateIpAddress=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.small --security-group-ids ${SEC_ID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${T_3[$i]}}]" --query 'Instance[0].PrivateIpAddress' --output text)
-    echo "DONE:::${T_3[$i]:PrivateIpAddress}"
+    echo "DONE:::${T_3[$i]}:${PrivateIpAddress}"
 done
