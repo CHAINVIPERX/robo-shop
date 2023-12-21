8a9ac2b91827789f6f75a4d4f305f4a1504f97e8
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-21 13:20:48 +0530

    q

--- testshell.sh
+++ testshell.sh
@@ -137,33 +137,34 @@
 #    rm -f "${filename}";
 #done <<< "${LOGSTODELETE}"
 
-name=""
-wishes="Hi!"
+# name=""
+# wishes="Hi!"
 
-usage(){
-    echo "Usage :: $(basename $0) -n <name> -w <wishes>"
-    echo "Options ::"
-    echo "-n, specify the name"
-    echo "-w, specify the wishes"
-    echo "-h, For help"
-}
-while getopts ":h:n:w:" opt;
-do
-    case $opt in
-        n) name="$OPTARG";;
-        w) wishes="$OPTARG";;
-        h) usage;exit ;;
-        \?) echo "Invalid Option ::$OPTARG" >&2; usage;exit;;
-        :) usage;exit ;;   
-    esac
-done
-#echo "Debug: name=$name, wishes=$wishes"
+# usage(){
+#     echo "Usage :: $(basename $0) -n <name> -w <wishes>"
+#     echo "Options ::"
+#     echo "-n, specify the name"
+#     echo "-w, specify the wishes"
+#     echo "-h, For help"
+# }
+# while getopts ":h:n:w:" opt;
+# do
+#     case $opt in
+#         n) name="$OPTARG";;
+#         w) wishes="$OPTARG";;
+#         h) usage;exit ;;
+#         \?) echo "Invalid Option ::$OPTARG" >&2; usage;exit;;
+#         :) usage;exit ;;   
+#     esac
+# done
+# #echo "Debug: name=$name, wishes=$wishes"
 
-if [ -z "$name" ];
-then
-    echo "ERROR:: -n and - w are mandaory"
-    usage
-    exit 1
-fi
-echo "${name} is a human"
-echo "${wishes} is a wish"
+# if [ -z "$name" ];
+# then
+#     echo "ERROR:: -n and - w are mandaory"
+#     usage
+#     exit 1
+# fi
+# echo "${name} is a human"
+# echo "${wishes} is a wish"
+ 
