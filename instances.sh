99adae53fb30ace21126e585451812cf8c45243b
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-21 15:42:36 +0530

    instances

--- instances.sh
+++ instances.sh
@@ -8,13 +8,13 @@ T_3=("MONGODB" "RABBITMQ" "SHIPPING" "MYSQL")
 for ((i=0;i < ${#T_2[*]};i++))
 do
     echo "Creating ${T_2[$i]} Instance"
-    PrivateIpAddress=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --security-group-ids ${SEC_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${T_2[$i]}}]" --query 'Instance[0].PrivateIpAddress' --output text)
+    PrivateIpAddress=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --security-group-ids ${SEC_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${T_2[$i]}}]" --query "Instances[0].PrivateIpAddress" --output text)
     echo "DONE:::${T_2[$i]}:${PrivateIpAddress}"
 done
 
 for ((i=0;i < ${#T_3[*]};i++))
 do
     echo "Creating ${T_3[$i]} Instance"
-    PrivateIpAddress=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.small --security-group-ids ${SEC_ID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${T_3[$i]}}]" --query 'Instance[0].PrivateIpAddress' --output text)
+    PrivateIpAddress=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.small --security-group-ids ${SEC_ID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${T_3[$i]}}]" --query "'Instances[0].PrivateIpAddress" --output text)
     echo "DONE:::${T_3[$i]}:${PrivateIpAddress}"
 done
