7f538e600c42a30a4b058eab5e464a175406ca62
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-21 15:25:09 +0530

    instances

--- instances.sh
+++ instances.sh
@@ -2,19 +2,19 @@
 
 AMI_ID="ami-03265a0778a880afb"
 SEC_ID="sg-070811a8d8f58361b"
-EC_2=("WEB" "USER" "CATALOGUE" "CART" "PAYMENTS" "REDIS")
-EC_3=("MONGODB" "RABBITMQ" "SHIPPING" "MYSQL")
+T_2=("WEB" "USER" "CATALOGUE" "CART" "PAYMENTS" "REDIS")
+T_3=("MONGODB" "RABBITMQ" "SHIPPING" "MYSQL")
 
-for ((i=0;i < ${#EC_2[*]};i++))
+for ((i=0;i < ${#T_2[*]};i++))
 do
-    echo "Creating ${EC_2[$i]} Instance"
-    aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --security-group-ids ${SEC_ID}
-    echo "DONE"
+    echo "Creating ${T_2[$i]} Instance"
+    PrivateIpAddress=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --security-group-ids ${SEC_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${T_2[$i]}}]" --query 'Instance[0].PrivateIpAddress' --output text)
+    echo "DONE:::${T_2[$i]:PrivateIpAddress}"
 done
 
-for ((i=0;i < ${#EC_3[*]};i++))
+for ((i=0;i < ${#T_3[*]};i++))
 do
-    echo "Creating ${EC_3[$i]} Instance"
-    aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.small --security-group-ids ${SEC_ID}
-    echo "DONE"
+    echo "Creating ${T_3[$i]} Instance"
+    PrivateIpAddress=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.small --security-group-ids ${SEC_ID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${T_3[$i]}}]" --query 'Instance[0].PrivateIpAddress' --output text)
+    echo "DONE:::${T_3[$i]:PrivateIpAddress}"
 done
