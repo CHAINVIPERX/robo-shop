29d05191910859ea3532eea994feae00f4b78ace
CHAINVIPERX <kbalajireddy111@gmail.com>
2023-12-21 14:40:35 +0530

    instances

--- instances.sh
+++ instances.sh
@@ -5,14 +5,14 @@ SEC_ID="sg-070811a8d8f58361b"
 EC_2=("WEB" "USER" "CATALOGUE" "CART" "PAYMENTS" "REDIS")
 EC_3=("MONGODB" "RABBITMQ" "SHIPPING" "MYSQL")
 
-for ((i=0;i <= ${#EC_2[*]};i++))
+for ((i=0;i < ${#EC_2[*]};i++))
 do
     echo "Creating ${EC_2[$i]} Instance"
     aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --security-group-ids ${SEC_ID}
     echo "DONE"
 done
 
-for ((i=0;i <= ${#EC_3[*]};i++))
+for ((i=0;i < ${#EC_3[*]};i++))
 do
     echo "Creating ${EC_3[$i]} Instance"
     aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.small --security-group-ids ${SEC_ID}
